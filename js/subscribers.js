7a372315bda2f2cfe153188e76366347825817d5
carlulita <81886298+carlulita@users.noreply.github.com>
2021-06-10 20:03:07 -0300

    Update subscribers.js

--- js/subscribers.js
+++ js/subscribers.js
@@ -1,122 +1,42 @@
-// function compra() {
-    
-//     const producto1 = 150
-//     const producto2 = 160
-//     const producto3 = 170
-//     const producto4 = 180
-//     const producto5 = 190
-//     const producto6 = 200
-    
-//     const adicional1 = 10
-//     const adicional2 = 20
-//     const adicional3 = 30
-    
-//     const guarnicion1 = 10
-//     const guarnicion2 = 20
-//     const guarnicion3 = 30
-    
-    
-//     let pedido =  prompt("Qué desea comer?")
+let btnSus = document.getElementById('btnSus')
 
-
-    
-    
-//     let subtotal = 0
-    
-    
-//     switch (pedido) {
-//         case '1' : producto1
-//         console.log(subtotal = subtotal + producto1)
-//         break;
-//         case '2' : producto2
-//         console.log(subtotal = subtotal + producto2)
-//         break;
-//         case '3' : producto3
-//         console.log(subtotal = subtotal + producto3)
-//         break;
-//         case '4' : producto4
-//         console.log(subtotal = subtotal + producto4)
-//         break;
-//         case '5' : producto5
-//         console.log(subtotal = subtotal + producto5)
-//         break;
-//         case '6' : producto6
-//         console.log(subtotal = subtotal + producto6)
-//         break; 
-//     }
-    
-//     let adicionales = prompt("¿Desea adicionales?")
-           
-//     switch (adicionales) {
-//         case '7': adicional1
-//         console.log(subtotal = subtotal + adicional1)
-//         break;
-//         case '8': adicional2
-//         console.log(subtotal = subtotal + adicional2)
-//         break;
-//         case '9': adicional3
-//         console.log(subtotal = subtotal + adicional3)
-//         break;
-//     }
-    
-//     let guarnicion =  prompt("¿Desea guarnicion?")
-
-//     switch (guarnicion) {
-//         case '10' : guarnicion1
-//         console.log(subtotal = subtotal + guarnicion1)
-//         break;
-//         case '11' : guarnicion2
-//         console.log(subtotal = subtotal + guarnicion2)
-//         break;
-//         case '12': guarnicion3
-//         console.log(subtotal = subtotal + guarnicion3)
-//         break;
-//     }
-
-// alert(subtotal)
-    
-//     }
-
-
-//--------------------------------------------------------------------------------------------------
-
-
-
+let subscribersList = []
 
 class Subscriber {
-    constructor(name, email, country) {
-         this.name = name
-         this.email = email
-         this.country = country
+    constructor(email) {
+        this.email = email
      }
-    
-      personSubscribed() {
-          alert(`¡Gracias ${this.name} por suscribirte al newsletter!`)
-       }
 }
 
-    function subscribe() {
-    
-    let subscribersList = []
-    
-    let name = prompt('Ingresá tu nombre.')
-    let email = prompt('Ingresá tu e-mail.')
-    let country = prompt('Ingresá tu país de residencia.')
-
-    subscribersList.push( new Subscriber(name, email, country)) 
-
-    console.log(subscribersList)
-
-    subscribersList[0].personSubscribed()
-}
-
-
-
-
-
-
-
-
-
-
-
+btnSus.addEventListener('click', ()=>{
+    let subscribersLS = JSON.parse(localStorage.getItem('subscribers'))
+    if(subscribersLS){
+        swal({
+            text: "Ya estás suscripto al newsletter",
+            icon: "warning",
+        }) 
+    } else {
+        swal({
+            title: "Ingresa tu correo electrónico",
+            text: "¡Suscribite al newsletter y recibí todas las novedades!",
+            content: "input",
+            icon: "info",
+            buttons: true,
+            dangerMode: true,
+            })
+        .then((value) => {
+            if(value.includes("@")) {
+                let newSubscriber = new Subscriber(value)
+                subscribersList.push(newSubscriber) 
+                localStorage.setItem('subscribers', JSON.stringify(subscribersList))
+                swal(`¡Te suscribiste correctamente al newsletter!`, {
+                icon: "success",
+                });
+            } else {
+                swal("Dirección de correo electrónico inválida", {
+                icon: "warning",
+                });
+            }
+            });
+        }
+})
